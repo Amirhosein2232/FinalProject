c78818b90cbb42197f489231eeeede6341483fbb
Amirhosein2232 <161626143+Amirhosein2232@users.noreply.github.com>
2024-06-13 16:12:41 +0330

    'guimade

--- CourtPieceProject/src/main/java/org/example/MainMenu.java
+++ CourtPieceProject/src/main/java/org/example/MainMenu.java
@@ -1,55 +1,202 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainMenu extends JFrame implements ActionListener {
-    JFrame accountOps;
-    JFrame signUpOps;
-    JFrame signInOps;
-    JFrame deleteOps;
+    JPanel mainPanel;
+    JButton newGame;
+    JButton accounts;
+    JLabel title;
+    JLabel playability;
+    ////////////////////////
+    JPanel accountsPanel;
+    JButton signUp;
+    JButton signIn;
+    JButton deleteAccount;
+    JButton cancel;
+    ////////////////////////
+    JPanel signUpPanel;
+    JLabel nameLabel1;
+    JTextField nameTextField1;
+    JLabel passwordLabel1;
+    JTextField passwordTextField1;
+    JLabel possibleError1;
+    JButton insert1;
+    JButton cancel1;
+    ////////////////////////
+    JPanel signInPanel;
+    JLabel nameLabel2;
+    JTextField nameTextField2;
+    JLabel passwordLabel2;
+    JTextField passwordTextField2;
+    JLabel possibleError2;
+    JButton insert2;
+    JButton cancel2;
     public MainMenu() {
+
+        mainPanel = new JPanel();
+        mainPanel.setSize(new Dimension(400,400));
+        mainPanel.setLayout(null);
+        mainPanel.setVisible(true);
+
+        title = new JLabel("Court Piece");
+        title.setBounds(10,50,100,50);
+
+        playability = new JLabel("You must sign in before playing the game.");
+        playability.setBounds(120,100,300,50);
+
+        newGame = new JButton("New Game");
+        newGame.setBounds(10,100,100,50);
+        newGame.addActionListener(this);
+        newGame.setEnabled(false);
+
+        accounts = new JButton("Accounts");
+        accounts.setBounds(10,175,100,50);
+        accounts.addActionListener(this);
+
+        mainPanel.add(title);
+        mainPanel.add(playability);
+        mainPanel.add(newGame);
+        mainPanel.add(accounts);
+        ////////////////////////////////////////////////////////////////////
+        accountsPanel = new JPanel();
+        accountsPanel.setSize(new Dimension(400,400));
+        accountsPanel.setLayout(null);
+        accountsPanel.setVisible(false);
+
+        signUp = new JButton("Sign Up");
+        signUp.setBounds(50,50,100,50);
+        signUp.addActionListener(this);
+
+        signIn = new JButton("Sign In");
+        signIn.setBounds(50,120,100,50);
+        signIn.addActionListener(this);
+
+        deleteAccount = new JButton("Delete Account");
+        deleteAccount.setBounds(50,190,100,50);
+        deleteAccount.addActionListener(this);
+        deleteAccount.setEnabled(false);
+
+        cancel = new JButton("Return");
+        cancel.setBounds(200,260,100,50);
+        cancel.addActionListener(this);
+
+        accountsPanel.add(signUp);
+        accountsPanel.add(signIn);
+        accountsPanel.add(deleteAccount);
+        accountsPanel.add(cancel);
+        ////////////////////////////////////////////////////////////////////
+        signUpPanel = new JPanel();
+        signUpPanel.setSize(new Dimension(400,400));
+        signUpPanel.setLayout(null);
+        signUpPanel.setVisible(false);
+
+        nameLabel1 = new JLabel("Username :");
+        nameLabel1.setBounds(50,50,100,20);
+
+        nameTextField1 = new JTextField();
+        nameTextField1.setBounds(50,90,250,20);
+
+        passwordLabel1 = new JLabel("Password :");
+        passwordLabel1.setBounds(50,130,100,20);
+
+        passwordTextField1 = new JTextField();
+        passwordTextField1.setBounds(50,170,250,20);
+
+        insert1 = new JButton("Insert");
+        insert1.addActionListener(this);
+        insert1.setBounds(50,210,100,20);
+
+        cancel1 = new JButton("Return");
+        cancel1.addActionListener(this);
+        cancel1.setBounds(200,210,100,20);
+
+        signUpPanel.add(nameLabel1);
+        signUpPanel.add(nameTextField1);
+        signUpPanel.add(passwordLabel1);
+        signUpPanel.add(passwordTextField1);
+        signUpPanel.add(insert1);
+        signUpPanel.add(cancel1);
+        ////////////////////////////////////////////////////////////////////
+        signInPanel = new JPanel();
+        signInPanel.setSize(new Dimension(400,400));
+        signInPanel.setLayout(null);
+        signInPanel.setVisible(false);
+
+        nameLabel2 = new JLabel("Username :");
+        nameLabel2.setBounds(50,50,100,20);
+
+        nameTextField2 = new JTextField();
+        nameTextField2.setBounds(50,90,250,20);
+
+        passwordLabel2 = new JLabel("Password :");
+        passwordLabel2.setBounds(50,130,100,20);
+
+        passwordTextField2 = new JTextField();
+        passwordTextField2.setBounds(50,170,250,20);
+
+        insert2 = new JButton("Insert");
+        insert2.addActionListener(this);
+        insert2.setBounds(50,210,100,20);
+
+        cancel2 = new JButton("Return");
+        cancel2.addActionListener(this);
+        cancel2.setBounds(200,210,100,20);
+
+        signInPanel.add(nameLabel2);
+        signInPanel.add(nameTextField2);
+        signInPanel.add(passwordLabel2);
+        signInPanel.add(passwordTextField2);
+        signInPanel.add(insert2);
+        signInPanel.add(cancel2);
+        ////////////////////////////////////////////////////////////////////
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
-        this.setSize(800,800);
+        this.setSize(400,400);
         this.setVisible(true);
         this.setResizable(false);
+
+        this.add(mainPanel);
+        this.add(accountsPanel);
+        this.add(signUpPanel);
+        this.add(signInPanel);
     }
-    public void accountsWindow() {
-        accountOps = new JFrame("Account Operations");
-        accountOps.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        accountOps.setLayout(null);
-        accountOps.setSize(800,800);
-        accountOps.setVisible(true);
-        accountOps.setResizable(false);
-    }
-    public void signUpWindow() {
-        signUpOps = new JFrame("Sign Up Operation");
-        signUpOps.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        signUpOps.setLayout(null);
-        signUpOps.setSize(800,800);
-        signUpOps.setVisible(true);
-        signUpOps.setResizable(false);
-    }
-    public void signInWindow() {
-        signInOps = new JFrame("Sign In Operation");
-        signInOps.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        signInOps.setLayout(null);
-        signInOps.setSize(800,800);
-        signInOps.setVisible(true);
-        signInOps.setResizable(false);
-    }
-    public void deleteAccountWindow() {
-        deleteOps = new JFrame("Delete Account Operation");
-        deleteOps.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        deleteOps.setLayout(null);
-        deleteOps.setSize(800,800);
-        deleteOps.setVisible(true);
-        deleteOps.setResizable(false);
+    public void getEntries() {
+        try {
+            
+        }
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == newGame) {
 
+        } else if (e.getSource() == accounts) {
+            mainPanel.setVisible(false);
+            accountsPanel.setVisible(true);
+        } else if (e.getSource() == cancel) {
+            accountsPanel.setVisible(false);
+            mainPanel.setVisible(true);
+        } else if (e.getSource() == signUp) {
+            accountsPanel.setVisible(false);
+            signUpPanel.setVisible(true);
+        } else if (e.getSource() == signIn) {
+            accountsPanel.setVisible(false);
+            signInPanel.setVisible(true);
+        } else if (e.getSource() == deleteAccount) {
+            System.out.println("deleted");
+        } else if (e.getSource() == insert1) {
+
+        } else if (e.getSource()== cancel1) {
+            signUpPanel.setVisible(false);
+            accountsPanel.setVisible(true);
+        } else if (e.getSource() == insert2) {
+
+        } else if (e.getSource() == cancel2) {
+            signInPanel.setVisible(false);
+            accountsPanel.setVisible(true);
+        }
     }
 }
